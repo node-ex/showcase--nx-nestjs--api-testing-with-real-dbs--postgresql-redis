e3aa7bebb54fa958702ebb67b269498044a306e0
Pavel Balashov <pavel.balashov1@gmail.com>
2024-12-04 01:01:09 +0100

    feat(app-nest-1): add caching to the coffees service

--- apps/app-nest-1/src/modules/coffees/services/coffees.service.ts
+++ apps/app-nest-1/src/modules/coffees/services/coffees.service.ts
@@ -3,20 +3,43 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CoffeeEntity } from '../entities/coffee.entity';
 import { Repository } from 'typeorm';
 import { CreateCoffeeDto } from '../dtos/create-coffee.dto';
+import { RedisService } from '@liaoliaots/nestjs-redis';
+import { Coffee } from '../types/coffee.interface';
 
 @Injectable()
 export class CoffeesService {
+  COFFEES_CACHE_KEY = 'coffees';
+
   constructor(
     @InjectRepository(CoffeeEntity)
     private readonly coffeeRepository: Repository<CoffeeEntity>,
+    private readonly redisService: RedisService,
   ) {}
 
-  getCoffees(): Promise<CoffeeEntity[]> {
-    return this.coffeeRepository.find();
+  async getCoffees(): Promise<Coffee[]> {
+    const connection = this.redisService.getOrThrow();
+    const cachedCoffees = await connection.get(this.COFFEES_CACHE_KEY);
+
+    if (cachedCoffees) {
+      console.log('Cache hit');
+      return JSON.parse(cachedCoffees) as Coffee[];
+    }
+
+    console.log('Cache miss');
+    const coffees = await this.coffeeRepository.find();
+    const coffeesToCache = JSON.stringify(coffees);
+    await connection.set(this.COFFEES_CACHE_KEY, coffeesToCache);
+
+    return coffees;
   }
 
-  createCoffee(createCoffeeDto: CreateCoffeeDto): Promise<CoffeeEntity> {
-    const coffee = this.coffeeRepository.create(createCoffeeDto);
-    return this.coffeeRepository.save(coffee);
+  async createCoffee(createCoffeeDto: CreateCoffeeDto): Promise<Coffee> {
+    let coffee = this.coffeeRepository.create(createCoffeeDto);
+    coffee = await this.coffeeRepository.save(coffee);
+
+    const connection = this.redisService.getOrThrow();
+    await connection.del(this.COFFEES_CACHE_KEY);
+
+    return coffee;
   }
 }
